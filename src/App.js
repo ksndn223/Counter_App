8f1bb9db58557b4df92419e59ead1940f025dab9
ksndn223 <11366993+ksndn223@users.noreply.github.com>
2023-12-25 02:39:22 -0500

    Updating App.js
    Adding the latest version of code.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style ={{
+      backgroundImage: `url(/mountains.png)`, backgroundSize:'cover',  backgroundRepeat: 'no-repeat'
+    }}>
+    <p> </p>
+     <h1> Counter check </h1>
+     <h3> Pressed the button to Increment Count at {count}</h3>
+    <button class="ml-auto" onClick={handleClick}>
+      Increment
+    </button>
     </div>
   );
-}
+ }
 
 export default App;
